48ff72b6de82765f3bf20eea592de5f0f396c365
jahnavi64 <84057163+jahnavi64@users.noreply.github.com>
2021-05-12 09:58:43 +0530

    Add files via upload

---
+++ prime1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n,count=0;
+	cin>>n;
+	bool isprime=true;
+	for(int i=2;i<sqrt(n);i++)
+	{
+		if(n%i==0)
+		{
+			count++;
+			cout<<"not prime";
+			break;
+		}
+	}
+	if(count==0)
+	{
+		cout<<"prime";
+	}
+		
+		
+	
+}
